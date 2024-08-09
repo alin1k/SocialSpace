a8c42debb64284650ceb66816e108595e0280bdd
Alin Olteanu <olteanualin1509@gmail.com>
2024-08-09 15:01:46 +0300

    Add github repo link to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="w-full bg-primary-light text-neutral p-4 text-center">
       <p>Web app designed and developed by <a href='https://github.com/alin1k' target='_blank' className='text-primary-dark'>Olteanu Alin</a> ©</p>
-      <p>Check out this project on Github <GitHubIcon className='ms-1'/> </p>
+      <p>Check out this project on <a href='https://github.com/alin1k/SocialSpace' target='_blank' className='text-primary-dark'>Github</a> <GitHubIcon className='ms-1'/> </p>
     </footer>
   )
 }
