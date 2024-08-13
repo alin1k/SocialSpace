dee9ddb283e939b5006746844f903157d83d3797
Alin Olteanu <olteanualin1509@gmail.com>
2024-08-13 13:46:21 +0300

    Add search by keyword feature

---
+++ src/app/posts/components/SearchField.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import SearchIcon from '@mui/icons-material/Search';
+import { useState, useEffect } from 'react';
+
+export default function SearchField({getSearchKeyword} : {getSearchKeyword: (formData: FormData)=>Promise<never>}) {
+  
+  const [isClinet, setIsClient] = useState(false);
+
+  useEffect(()=>{
+    setIsClient(true);
+  }, [])
+  
+  return (
+    <form action={getSearchKeyword} className="mt-3">
+      {isClinet && <SearchIcon/>}
+      <label htmlFor="search" className='text-lg'>Search posts by keyword</label>
+      <div className="flex flex-row flex-nowrap">
+        <input autoComplete="off" name="search" type="text" className="mt-1 w-full border rounded-xl p-2 rounded-e-none" placeholder="ex: love" />
+        <button className="mt-1 p-1 px-2 border rounded-xl hover:bg-primary rounded-s-none">Search</button>
+      </div>
+    </form>
+  )
+}
+ 
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,9 @@
 import { Metadata } from "next"
+import { redirect } from "next/navigation"
 import TagList from "./components/TagList"
 import Post from "@/components/Post"
 import { PostType } from "@/types/types"
+import SearchField from "./components/SearchField"
 
 export const metadata : Metadata = {
   title: "Posts"
@@ -23,27 +25,63 @@ const getPostsByTag = async (tag : string) =>{
   return res.json();
 }
 
-export default async function Posts({searchParams} : {searchParams: {tags: string}}){
+const getPostsBySearchKeyword = async (keyword: string) =>{
+  const res = await fetch(`https://dummyjson.com/posts/search?q=${keyword}&limit=0`, {
+    cache: 'no-store'
+  }) 
+
+  return res.json();
+}
+
+const getSearchKeyword = async (formData: FormData)=>{
+  "use server"
+
+  const keyword = formData.get("search"); 
+  redirect(`/posts/?search=${keyword}`)
+}
+
+const isSearchParamsEmpty = (searchParams: {tags? : string, search? : string}) : boolean =>{
+  return (
+    searchParams &&
+    Object.keys(searchParams).length === 0
+  )
+}
+
+const searchPosts = async (searchParams : {tags?: string | undefined, search?: string | undefined}) =>{
+  if(isSearchParamsEmpty(searchParams)) return [];
+  if(searchParams.tags) return await getPostsByTag(searchParams.tags);
+  if(searchParams.search) return await getPostsBySearchKeyword(searchParams.search);
+}
+
+export default async function Posts({searchParams} : {searchParams: {tags?: string | undefined, search?: string}}){
 
   const tags : string[] = await getAllTags();
-  const {posts} : {posts: PostType[]} = await getPostsByTag(searchParams.tags);
+  const {posts} : {posts: PostType[]} = await searchPosts(searchParams);
 
   return (
     <div>
       {searchParams.tags? 
         <h1 className="text-2xl font-semibold text-center">Search for <span className="bg-primary-light p-1">#{searchParams.tags}</span> posts</h1> 
-        : 
+        :
         <h1 className="text-2xl font-semibold text-center">Search for posts</h1>
       }
 
-      <div className="border rounded-xl p-3 mt-3">
-        <p className="text-lg">Search posts by tags #</p>
-        <TagList tags={tags} /> 
-      </div>
+      {isSearchParamsEmpty(searchParams)? 
+        <div>
+          <div className="border rounded-xl p-3 mt-3">
+            <p className="text-lg">Search posts by tags #</p>
+            <TagList tags={tags} /> 
+          </div>
+          <SearchField getSearchKeyword={getSearchKeyword}/>
+        </div>
+        :
+        <></>
+      }
+
 
       <hr className="my-5"/>
 
-      {posts && searchParams.tags ? 
+      {!isSearchParamsEmpty(searchParams) ? 
         <h1 className="mb-5">Found {posts.length} posts</h1>
         :
         <></>
