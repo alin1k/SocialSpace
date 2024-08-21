5c18484822716f8f89bec9daac8ee89053f070cf
Alin Olteanu <olteanualin1509@gmail.com>
2024-08-21 17:51:39 +0300

    Edit comments feature

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -6,9 +6,12 @@ import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlin
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import { useUserCommentsContext } from "@/context/comments";
 import EditButton from "./EditButton";
+import { Dispatch, SetStateAction } from "react";
 
 
 export default function Comment({comment} : {comment: CommentType}) {
+
+  const [editState, setEditState] = useState(false);
 
   const [isClient, setIsClient] = useState(false);
   useEffect(()=>{
@@ -37,11 +40,61 @@ export default function Comment({comment} : {comment: CommentType}) {
           {isClient && <AccountCircleOutlinedIcon className="inline size-6"/>}
           <p className="font-semibold inline ms-1">{comment.user.username}</p>
         </div>
-        {(isClient && userId == comment.user.id )&& <EditButton deleteAction={handleDeleteComment}/>}
+        {(isClient && userId == comment.user.id )&& <EditButton deleteAction={handleDeleteComment} editAction={setEditState}/>}
       </div>
-      <p>{comment.body}</p>
+
+      {editState?
+        <EditComment comment={comment} setEditState={setEditState} setUserComments={setUserComments}/>
+        :
+        <p>{comment.body}</p>
+      }
+      
       {isClient && <FavoriteBorderOutlinedIcon className="size-4 inline"/>}
       <p className="inline ms-1 text-xs">{comment.likes}</p>
+    </div>
+  )
+}
+
+function EditComment({
+  comment, setEditState , setUserComments
+} : {
+  comment: CommentType, 
+  setEditState: Dispatch<SetStateAction<boolean>>,
+  setUserComments: Dispatch<SetStateAction<CommentType[] | undefined>>
+}){
+
+  const [inputValue, setInputValue] = useState(comment.body);
+
+  return (
+    <div className="block">
+      <input 
+        type="text" 
+        value={inputValue} 
+        onChange={(e)=>{
+          e.preventDefault();
+          setInputValue(e.target.value);
+        }}
+        className="w-full border rounded p-1"
+      />
+      <button 
+        className="border p-1 px-2 mt-1 rounded hover:bg-primary"
+        onClick={()=>{
+          setEditState(false)
+          setUserComments((prev)=>{
+            let currentComments = prev ?? [];
+            return currentComments.map((comm)=> comm.id===comment.id? {...comm, body: inputValue,} : comm);
+          })
+        }}
+      >
+        Confirm edit
+      </button>
+      <button 
+        className="ms-3 border p-1 px-2 rounded bg-red-300 hover:bg-red-500"
+        onClick={()=> setEditState(false)}
+      >
+        Cancel
+      </button>
+
     </div>
   )
 }
--- src/components/EditButton.tsx
+++ src/components/EditButton.tsx
@@ -2,8 +2,9 @@
 
 import { useState, useEffect, useRef } from 'react';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Dispatch, SetStateAction } from 'react';
 
-function EditButton({deleteAction} : {deleteAction: ()=>void}) {
+function EditButton({deleteAction, editAction} : {deleteAction: ()=>void, editAction: Dispatch<SetStateAction<boolean>>}) {
 
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
@@ -44,13 +45,16 @@ function EditButton({deleteAction} : {deleteAction: ()=>void}) {
       {isOpen && (
         <div
           ref={dropdownRef}
-          className="absolute left-1/2 transform -translate-x-1/2 mt-2 w-24 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10"
+          className="absolute left-0 transform -translate-x-1/2 mt-2 w-24 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10"
           style={{ top: "auto", bottom: "100%" }}
         >
           <div className="py-1">
             <button
               className="block w-full text-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:outline-none"
-              onClick={() => setIsOpen(false)}
+              onClick={() => {
+                editAction(true);
+                setIsOpen(false)
+              }}
             >
               Edit
             </button>
